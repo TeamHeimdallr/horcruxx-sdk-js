f5e6ffad697f8e208acb268a04d6a43fc86e7f2d
Imchaemin <chaaaamni@gmail.com>
2022-12-18 01:20:30 +0900

    fix: moduler size

--- rollup.config.js
+++ rollup.config.js
@@ -5,71 +5,76 @@ import babel from '@rollup/plugin-babel';
 import json from '@rollup/plugin-json';
 import typescript from 'rollup-plugin-typescript2';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+import { terser } from 'rollup-plugin-terser';
+import sourcemaps from 'rollup-plugin-sourcemaps';
 import path from 'path';
 
+import pkg from './package.json';
+
 const extensions = ['.js', '.ts'];
+
+const plugins = [
+  alias({
+    entries: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
+  }),
+  commonjs(),
+  resolve({ extensions, preferBuiltins: true }),
+  peerDepsExternal(),
+  json(),
+  typescript({
+    clean: true,
+    sourceMap: false,
+    noEmitOnError: true,
+    tsconfig: './tsconfig.json',
+  }),
+  babel({
+    // babelHelpers: 'runtime',
+    babelHelpers: 'bundled',
+    exclude: 'node_modules/**',
+    // extensions,
+    // skipPreflightCheck: true,
+  }),
+  sourcemaps(),
+  terser(),
+];
+
+const treeshake = {
+  moduleSideEffects: false,
+};
+
+const external = ['web3'];
 
 export default [
   // esm 번들링
   {
     input: 'src/index.ts',
     output: [
-      { file: 'dist/esm/index.js', format: 'esm', exports: 'auto' },
-      { file: 'dist/es/index.js', format: 'esm', exports: 'auto' },
+      {
+        file: pkg.module,
+        format: 'esm',
+        exports: 'auto',
+        sourcemap: true,
+      },
     ],
-    external: [/@babel\/runtime/],
-    plugins: [
-      typescript({
-        sourceMap: false,
-        noEmitOnError: true,
-        tsconfig: './tsconfig.json',
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-        extensions,
-        skipPreflightCheck: true,
-      }),
-      alias({
-        entries: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-      }),
-      commonjs({ extensions: [...extensions, '.js'] }),
-      resolve({ extensions, preferBuiltins: true }),
-      peerDepsExternal(),
-      json(),
-    ],
+    // external: [/@babel\/runtime/],
+    external,
+    treeshake,
+    plugins,
   },
   // cjs 번들링
   {
     input: 'src/index.ts',
     output: [
       {
-        dir: 'dist/cjs',
+        file: pkg.main,
         format: 'cjs',
         exports: 'auto',
-        preserveModules: true,
+        sourcemap: true,
       },
     ],
-    external: [/@babel\/runtime/],
-    plugins: [
-      typescript({
-        sourceMap: false,
-        noEmitOnError: true,
-        tsconfig: './tsconfig.json',
-      }),
-      babel({
-        babelHelpers: 'runtime',
-        exclude: 'node_modules/**',
-        extensions,
-        skipPreflightCheck: true,
-      }),
-      alias({
-        entries: [{ find: '~', replacement: path.resolve(__dirname, 'src') }],
-      }),
-      commonjs({ extensions: [...extensions, '.js'] }),
-      resolve({ extensions }),
-      peerDepsExternal(),
-      json(),
-    ],
+    // external: [/@babel\/runtime/],
+    external,
+    treeshake,
+    plugins,
   },
 ];
