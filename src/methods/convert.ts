308908d081637c87b8c2e016c52919c91fb59041
koe7 <taekeongo@gmail.com>
2022-12-19 00:18:59 +0900

    wip: convert nft method

--- src/methods/convert.ts
+++ src/methods/convert.ts
@@ -16,18 +16,12 @@ export interface ConvertFromNFT {
 export const convertFromNFT = async (params: ConvertFromNFTParams): Promise<ConvertFromNFT> => {
   const nftContract = getContract(params.nftAddress);
 
-  const val = web3.eth.abi.encodeParameters(
-    ['address', 'address', 'uint256'],
-    [getAccount().address, params.sbtAddress, web3.utils.toHex(params.tokenId)],
-  );
-  console.log(val);
-  // console.log(decodeUint256(new BN(params.tokenId));
-
   verfiyAccount();
 
-  // const encoded = nftContract.methods.safeTransferFrom(getAccount().address, params.sbtAddress, val).encodeABI();
-  const encoded = nftContract.methods.safeTransferFrom(val).encodeABI();
-  await signAndSendTx({ to: params.sbtAddress, data: encoded, account: getAccount() });
+  const encoded = nftContract.methods
+    .safeTransferFrom(getAccount().address, params.sbtAddress, web3.eth.abi.encodeParameter('uint256', params.tokenId))
+    .encodeABI();
+  await signAndSendTx({ to: params.nftAddress, data: encoded, account: getAccount() });
 
   return {
     success: true,
