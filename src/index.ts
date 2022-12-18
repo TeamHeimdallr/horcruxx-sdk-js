5f437197ef33a62fdc1f833a49d7b80dcec39530
Imchaemin <chaaaamni@gmail.com>
2022-12-19 02:52:50 +0900

    fix: index ts

--- src/index.ts
+++ src/index.ts
@@ -1,62 +1,11 @@
-export {
-  approve,
-  ApprovedAddress,
-  ApprovedForAll,
-  ApproveParams,
-  getApproved,
-  GetApprovedParams,
-  isApprovedForAll,
-  IsApprovedForAllParams,
-  setApprovalForAll,
-  SetApprovelForAllParams,
-} from './methods/approve';
-export { Balance, balanceOf, BalanceOfParams } from './methods/balance';
-export { ConvertFromNFT, convertFromNFT, ConvertFromNFTParams } from './methods/convert';
-export { DeploySBTContract, deploySBTContract, DeploySBTContractParams } from './methods/deploy';
-export {
-  TokenByIndex,
-  tokenByIndex,
-  TokenByIndexParams,
-  TokenOfOwnerByIndex,
-  tokenOfOwnerByIndex,
-  TokenOfOwnerByIndexParams,
-  TotalSupply,
-  totalSupply,
-} from './methods/enumerable';
-export {
-  Name,
-  name,
-  NameParams,
-  symbol,
-  SymbolParams,
-  TokenSymbol,
-  TokenUri,
-  tokenUri,
-  TokenUriParams,
-} from './methods/metadata';
-export { Owner, ownerOf, OwnerOfParams } from './methods/owner';
-export {
-  BurnerAddress,
-  BurnerAddressParams,
-  CollectStats,
-  CollectStatsParams,
-  getBurnerAddress,
-  getCollectStats,
-  getNFTAddressBySBT,
-  getSBTAddressByNFT,
-  getSBTAttributes,
-  getSBTCollectionMetadata,
-  getSBTMetadata,
-  NFTAddressBySBT,
-  NFTAddressBySBTParams,
-  SBTAddressByNFT,
-  SBTAddressByNFTParams,
-  SBTAttribute,
-  SBTAttributes,
-  SBTAttributesParams,
-  SBTCollectionMetadata,
-  SBTCollectionMetadataParams,
-  SBTMetadata,
-  SBTMetadataParams,
-} from './methods/soulbound-token';
-export { safeTransferFrom, SafeTransferFromParmas, transferFrom, TransferFromParmas } from './methods/transfer';
+export * from './methods/approve';
+export * from './methods/balance';
+export * from './methods/connect';
+export * from './methods/convert';
+export * from './methods/deploy';
+export * from './methods/enumerable';
+export * from './methods/lock';
+export * from './methods/metadata';
+export * from './methods/soulbound-token';
+export * from './methods/transfer';
+export * from './types';
