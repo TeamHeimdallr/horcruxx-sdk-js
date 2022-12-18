2486df0a555b1b794c9f0e1af5dded0e6ecf945e
Imchaemin <chaaaamni@gmail.com>
2022-12-19 02:55:48 +0900

    fix: build config

--- rollup.config.js
+++ rollup.config.js
@@ -40,34 +40,18 @@ const external = ['web3'];
 
 export default [
   {
-    input: 'src/sbt/index.ts',
+    input: 'src/index.ts',
     output: [
-      { file: 'dist/sbt/index.esm.js', format: 'esm', exports: 'auto', sourcemap: true },
-      { file: 'dist/sbt/index.cjs.js', format: 'cjs', exports: 'auto', sourcemap: true },
+      { file: 'dist/index.esm.js', format: 'esm', exports: 'auto', sourcemap: true },
+      { file: 'dist/index.cjs.js', format: 'cjs', exports: 'auto', sourcemap: true },
     ],
     external,
     treeshake,
     plugins,
   },
   {
-    input: 'src/sbt/index.ts',
-    output: [{ file: 'dist/sbt/index.d.ts', format: 'esm' }],
-    plugins: [dts.default(), alias({ entries: [{ find: '~', replacement: path.resolve(__dirname, 'src') }] })],
-  },
-
-  {
-    input: 'src/nft/index.ts',
-    output: [
-      { file: 'dist/nft/index.esm.js', format: 'esm', exports: 'auto', sourcemap: true },
-      { file: 'dist/nft/index.cjs.js', format: 'cjs', exports: 'auto', sourcemap: true },
-    ],
-    external,
-    treeshake,
-    plugins,
-  },
-  {
-    input: 'src/nft/index.ts',
-    output: [{ file: 'dist/nft/index.d.ts', format: 'esm' }],
+    input: 'src/index.ts',
+    output: [{ file: 'dist/index.d.ts', format: 'esm' }],
     plugins: [dts.default(), alias({ entries: [{ find: '~', replacement: path.resolve(__dirname, 'src') }] })],
   },
 ];
