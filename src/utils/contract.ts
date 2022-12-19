0b418a714b22abfd4983af8f47c3ed1a46d57b96
koe7 <taekeongo@gmail.com>
2022-12-19 09:45:52 +0900

    fix: request accounts when metamask

--- src/utils/contract.ts
+++ src/utils/contract.ts
@@ -31,9 +31,13 @@ export const signAndSendTx = async ({ to, data, gas }: SignAndSendTxParams): Pro
         data,
       };
 
+  console.log(web3.eth.currentProvider);
   const account = getAccount();
-  if (account == undefined) {
-    let signedTx = await web3.eth.signTransaction(tx);
+  if (account == undefined && (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined')) {
+    const ethAccounts = await web3.eth.requestAccounts();
+
+    console.log(ethAccounts);
+    let signedTx = await web3.eth.signTransaction(tx, ethAccounts[0]);
     return web3.eth.sendSignedTransaction(signedTx.raw).on('error', console.error);
   } else {
     let signedTx = await web3.eth.accounts.signTransaction(tx, account.privateKey);
