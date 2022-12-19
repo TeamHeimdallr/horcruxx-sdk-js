e164a7256b22291f3c1acc83d1ec1fae2107f4c3
koe7 <taekeongo@gmail.com>
2022-12-19 09:54:12 +0900

    fix: getAccount when undefined

--- src/methods/convert.ts
+++ src/methods/convert.ts
@@ -15,8 +15,17 @@ export interface ConvertFromNFT {
 export const convertFromNFT = async (params: ConvertFromNFTParams): Promise<ConvertFromNFT> => {
   const nftContract = getContract(params.nftAddress);
 
+  const account = getAccount();
+  let accountAddress: string;
+  if (account == undefined && (typeof window.ethereum !== 'undefined' || typeof window.web3 !== 'undefined')) {
+    const ethAccounts = await web3.eth.requestAccounts();
+    accountAddress = ethAccounts[0];
+  } else {
+    accountAddress = account.address;
+  }
+
   const encoded = nftContract.methods
-    .safeTransferFrom(getAccount().address, params.sbtAddress, web3.eth.abi.encodeParameter('uint256', params.tokenId))
+    .safeTransferFrom(accountAddress, params.sbtAddress, web3.eth.abi.encodeParameter('uint256', params.tokenId))
     .encodeABI();
   await signAndSendTx({ to: params.nftAddress, data: encoded });
 
